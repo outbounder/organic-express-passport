003bb416fda99b85edab5a53872e41c3102c9176
Boris Filipov <obiwonn@gmail.com>
2014-08-18 18:41:17 +0300

    expected initPassport.js script execution result is an init function, release 0.1.0

--- test/data/initPassport.js
+++ test/data/initPassport.js
@@ -1,7 +1,9 @@
-var passport = require("passport")
-var LocalStrategy = require('passport-local').Strategy;
-var User = require("./mongooseUser")
+module.exports = function(){
+  var passport = require("passport")
+  var LocalStrategy = require('passport-local').Strategy;
+  var User = require("./mongooseUser")
 
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+  passport.use(new LocalStrategy(User.authenticate()))
+  passport.serializeUser(User.serializeUser())
+  passport.deserializeUser(User.deserializeUser())  
+}
